78d409ad8a3cc6d392d48ee3a85f53fd0bccb2f1
Azat Mardan <hi@azat.co>
2014-09-15 09:41:53 -0700

    make code prettier

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
-var express = require('express')
-  , mongoskin = require('mongoskin')
-  , bodyParser = require('body-parser')
+var express = require('express'),
+  mongoskin = require('mongoskin'),
+  bodyParser = require('body-parser')
 
 var app = express()
 app.use(bodyParser())
@@ -17,7 +17,7 @@ app.get('/', function(req, res, next) {
 })
 
 app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
@@ -38,16 +38,16 @@ app.get('/collections/:collectionName/:id', function(req, res, next) {
 })
 
 app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+  req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result){
     if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
+    res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
   })
 })
 
 app.delete('/collections/:collectionName/:id', function(req, res, next) {
   req.collection.removeById(req.params.id, function(e, result){
     if (e) return next(e)
-    res.send((result===1)?{msg:'success'}:{msg:'error'})
+    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
   })
 })
 
